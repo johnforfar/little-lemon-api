04b9ebda0d718776060db99f54706d6fab236a57
john-norg <john.forfar@gmail.com>
2024-03-02 00:14:08 +0800

    Create serializers.py

---
+++ LittleLemonAPI/serializers.py
@@ -0,0 +1,14 @@
+# ./LittleLemonAPI/serializers.py
+
+from rest_framework import serializers
+from .models import Category, MenuItem
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = '__all__'
