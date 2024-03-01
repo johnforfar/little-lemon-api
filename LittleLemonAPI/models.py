244489414741b5f9a64e496d1ab66b3b93c78762
john-norg <john.forfar@gmail.com>
2024-03-02 00:10:16 +0800

    models added

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+# ./LittleLemonAPI/models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    category = models.ForeignKey(Category, related_name='menu_items', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.category.name}"
+
